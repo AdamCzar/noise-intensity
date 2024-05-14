fc3f276bcb8127b80bb352447fbc07ebadbaec75
Adam Czarnecki <adamczarnecki@Adams-MacBook-Air.local>
2024-05-14 13:32:34 -0400

    edit timeout

--- waveplate_control.py
+++ waveplate_control.py
@@ -50,7 +50,7 @@ def sweep(controller, start, end, step):
     
 def move(controller, position):
     print('Step to ' + str(position))
-    controller.MoveTo(Decimal(position), 600000)
+    controller.MoveTo(Decimal(position), int(60000))
     time.sleep(.25)
     
 def step(controller, step):
