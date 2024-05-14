f58ab49990a0312b33154d47529fba585da5b57d
Adam Czarnecki <adamczarnecki@Adams-MacBook-Air.local>
2024-05-14 13:29:14 -0400

    modified thorlabs connection procedure

--- noise_intensity_sweep.py
+++ noise_intensity_sweep.py
@@ -22,37 +22,44 @@ thor.configure_Thorlabs()
 
 serial_num = str('83835052') # use S/N of T Cube controller
 controller = thor.connect_controller(serial_num)
-thor.activate(controller, serial_num)
+
+if not controller == None: # check if connection worked
+    controller.Connect(serial_num)
+
+    if not controller.IsSettingsInitialized(): # wait for connection and initialization
+        controller.WaitForSettingsInitialized(3000) # in units of ms
+
+    thor.activate(controller, serial_num)
 
 
-# Conduct sweep
-print('Moving Motor to start')
-#controller.MoveTo(Decimal(start), 60000) # immediately continue
-thor.move(controller, start_angle)
-time.sleep(2)
+    # Conduct sweep
+    print('Moving Motor to start')
+    #controller.MoveTo(Decimal(start), 60000) # immediately continue
+    thor.move(controller, start_angle)
+    time.sleep(2)
 
-# Sweep specified range
-n = int((end_angle - start_angle)/step_angle)
-print('Sweeping')
-for i in range(n):
-    #current_pos = controller.Position.ToString()
-    #controller.MoveTo(Decimal(current_pos) + Decimal(step), 60000)
-    thor.step(controller, step_angle)
-    #time.sleep(0.25)
-    
-    #Obtain data with triggering disabled
-    data_no_trig = scope.get_scope_records(scope.scope_module, n_records)
-    
-    v, n = scope.extract_stats(data_no_trig)
-    voltages.append(v)
-    noises.append(n)
-    
+    # Sweep specified range
+    n = int((end_angle - start_angle)/step_angle)
+    print('Sweeping')
+    for i in range(n):
+        #current_pos = controller.Position.ToString()
+        #controller.MoveTo(Decimal(current_pos) + Decimal(step), 60000)
+        thor.step(controller, step_angle)
+        #time.sleep(0.25)
 
-print('Finished Sweeping')
+        #Obtain data with triggering disabled
+        data_no_trig = scope.get_scope_records(scope.scope_module, n_records)
+
+        v, n = scope.extract_stats(data_no_trig)
+        voltages.append(v)
+        noises.append(n)
 
 
-# Disconnect Thorlabs stage once sweep is complete
-thor.close_controller(controller)
+    print('Finished Sweeping')
+
+
+    # Disconnect Thorlabs stage once sweep is complete
+    thor.close_controller(controller)
 
 plt.scatter(voltages, noises)
 plt.xlabel('Voltage (V)')
--- waveplate_control.py
+++ waveplate_control.py
@@ -62,52 +62,47 @@ def step(controller, step):
     
 def activate(controller, serial_num):
     
-    if not controller == None: # check if connection worked
-        controller.Connect(serial_num)
         
-        if not controller.IsSettingsInitialized(): # wait for connection and initialization
-            controller.WaitForSettingsInitialized(3000) # in units of ms
-        
-        controller.StartPolling(50) # instruct controller to send updates about position and motor status to PC with update rate of 50ms
-        time.sleep(.1) # give controller time to update, good habits to set as twice the duration of polling rate
-        controller.EnableDevice()
-        time.sleep(.1)
-        
-        # load parameters for translation stage into the TCube
-        config = controller.LoadMotorConfiguration(serial_num, DeviceConfiguration.DeviceSettingsUseOptionType.UseFileSettings)
-        config.DeviceSettingsName = str('MTS50-Z8') # use part number of this stage
-        config.UpdateCurrentConfiguration()
-        controller.SetSettings(controller.MotorDeviceSettings, True, False)
-        info = controller.GetDeviceInfo()
-        
-        print(f"Controller {serial_num} = {info.Name}")
+    controller.StartPolling(50) # instruct controller to send updates about position and motor status to PC with update rate of 50ms
+    time.sleep(.1) # give controller time to update, good habits to set as twice the duration of polling rate
+    controller.EnableDevice()
+    time.sleep(.1)
 
-        #print('Homing Motor')
-        #controller.Home(60000) # if command does not complete by the end of this time, it will throw an error
-        
-        # to change the jog params of the translation stage, first import them with the GetJogParams method and then modify
-        jog_params = controller.GetJogParams()
-        jog_params.StepSize = Decimal(1) # units of deg
-        jog_params.MaxVelocity = Decimal(25) # units in deg/s
-        jog_params.JogMode = JogParametersBase.JogModes.SingleStep
-        
-        # send updated jog params back to the controller
-        controller.SetJogParams(jog_params)
-        
-        print(controller.Position.ToString())
-        
-        # Jog motor single step
-        #print('Jogging Motor')
-        #controller.MoveJog(MotorDirection.Forward, 60000)
-        
-        #print(controller.Position.ToString())
-        
-        # Move to user position
-        #print('Moving Motor')
-        #controller.MoveTo(Decimal(user_position), 0) # immediately continue
-        #time.sleep(.25)
-        
-        #print(controller.Position.ToString())
+    # load parameters for translation stage into the TCube
+    config = controller.LoadMotorConfiguration(serial_num, DeviceConfiguration.DeviceSettingsUseOptionType.UseFileSettings)
+    config.DeviceSettingsName = str('MTS50-Z8') # use part number of this stage
+    config.UpdateCurrentConfiguration()
+    controller.SetSettings(controller.MotorDeviceSettings, True, False)
+    info = controller.GetDeviceInfo()
+
+    print(f"Controller {serial_num} = {info.Name}")
+
+    #print('Homing Motor')
+    #controller.Home(60000) # if command does not complete by the end of this time, it will throw an error
+
+    # to change the jog params of the translation stage, first import them with the GetJogParams method and then modify
+    jog_params = controller.GetJogParams()
+    jog_params.StepSize = Decimal(1) # units of deg
+    jog_params.MaxVelocity = Decimal(25) # units in deg/s
+    jog_params.JogMode = JogParametersBase.JogModes.SingleStep
+
+    # send updated jog params back to the controller
+    controller.SetJogParams(jog_params)
+
+    print(controller.Position.ToString())
+
+    # Jog motor single step
+    #print('Jogging Motor')
+    #controller.MoveJog(MotorDirection.Forward, 60000)
+
+    #print(controller.Position.ToString())
+
+    # Move to user position
+    #print('Moving Motor')
+    #controller.MoveTo(Decimal(user_position), 0) # immediately continue
+    #time.sleep(.25)
+
+    #print(controller.Position.ToString())
         
         
         
